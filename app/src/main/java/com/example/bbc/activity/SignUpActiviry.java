dcb622a942ed4c692580bdcae985cfbdbd34d07f
Dana Moradi <moradi.dana100@gmail.com>
2023-03-19 22:10:58 +0330

    set packages & coloe & depenceis

---
+++ app/src/main/java/com/example/bbc/activity/SignUpActiviry.java
@@ -0,0 +1,16 @@
+package com.example.bbc.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bbc.R;
+
+public class SignUpActiviry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_activiry);
+    }
+}
